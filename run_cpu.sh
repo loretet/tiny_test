9a58ce4afca0ee713109f100d73415e6685a8c39
Lorenzo Donati <93974114+loretet@users.noreply.github.com>
2026-04-17 17:41:23 +0200

    add comments about modules

--- run_cpu.sh
+++ run_cpu.sh
@@ -7,6 +7,12 @@
 #SBATCH -A naiss2025-1-5
 #SBATCH --mail-type=BEGIN,END,FAIL
 
+# LUMI:
+# ml CrayEnv cpe/23.09 craype-accel-amd-gfx90a rocm
+
+# Dardel:
+ml PrgEnv-cray rocm/6.2.4 craype-accel-amd-gfx90a
+
 if [ ! -d logfiles ]; then
     mkdir logfiles
 fi
